a94eba23d87989cbd71742522134a2977b86d1f3
unaivan <unaivan22@gmail.com>
2021-01-18 08:16:24 +0700

    upload

---
+++ README.md
@@ -0,0 +1,11 @@
+# Pendek.in
+Shorten your url
+
+
+## Preview
+<img src="https://raw.githubusercontent.com/unaivan22/pendekIn/master/media/pendekin.png">
+
+## Dribbble
+https://dribbble.com/shots/14951515-Pendek-in-Shorten-your-url
+
+
---
+++ js/index.js
@@ -0,0 +1,15 @@
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+document.getElementById("randomString").innerHTML = makeid(6);
+
+function shoternUrl(){
+    document.getElementById('content').style.display = "block";
+    makeid();
+}
